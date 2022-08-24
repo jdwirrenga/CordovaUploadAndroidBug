6ff73e2d8e57f841cda5826305b957f3a246b79c
Jaden Wirrenga <jaden.wirrenga@onsharp.com>
2022-08-24 16:08:39 -0500

    initial project

---
+++ cordova-upload-android-bug/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CordovaUploadAndroidBug',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
